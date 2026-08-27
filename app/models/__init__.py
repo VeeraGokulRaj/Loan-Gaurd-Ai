794f6a1528f1f8dc621b532cfade84a5db9833eb
VeeraGokulRaj <110714512+VeeraGokulRaj@users.noreply.github.com>
2026-08-27 22:41:37 +0530

    feat: Add models relate to Module B (#11)
    Added the models ValidationRule and LoanException

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .audit import *  # noqa: F403
 from .base import *  # noqa: F403
 from .ingestion import *  # noqa: F403
 from .user import *  # noqa: F403
+from .validation import *  # noqa: F403
